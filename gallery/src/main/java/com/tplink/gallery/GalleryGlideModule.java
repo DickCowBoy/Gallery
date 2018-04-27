99419bfd864098c2ab419cac35a89b61586bb136
Lin Jinlong <linjinlong@tp-link.com.cn>
2018-04-27 15:05:49 +0800

    TPGallery2: use Hardware Bitmaps to promote memory usage
    描述:使用硬件bitmap减少内存消耗

--- gallery/src/main/java/com/tplink/gallery/GalleryGlideModule.java
+++ gallery/src/main/java/com/tplink/gallery/GalleryGlideModule.java
@@ -1,10 +1,22 @@
 package com.tplink.gallery;
 
+import android.content.Context;
+
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.module.AppGlideModule;
 
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
 
 @GlideModule
-public final class GalleryGlideModule extends AppGlideModule {}
+public final class GalleryGlideModule extends AppGlideModule {
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        // 使用Hardware Bitmaps减少内存使用
+        builder.setDefaultRequestOptions(new RequestOptions().
+                format(DecodeFormat.PREFER_ARGB_8888).disallowHardwareConfig());
+    }
+}
